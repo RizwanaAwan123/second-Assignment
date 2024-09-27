6cab525f443646d79b25ce44fe56b8439efb3b9f
RizwanaAwan123 <awanareeb34@gmail.com>
2024-09-27 22:51:13 +0500

    first commit

---
+++ components/app/page.tsx
@@ -0,0 +1,12 @@
+import About from "./components/components/about/page";
+import Header from "./components/components/about/page";
+
+const Home = ()  => {
+  return (
+    <div>
+      <Header/>
+      <About/>
+    </div>
+  );
+}
+export default Home
